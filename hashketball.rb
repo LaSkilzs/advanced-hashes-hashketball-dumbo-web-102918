8474e3a0eb0c1538ed54a212f14010f6d16f3c68
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-14 17:28:16 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,9 +150,9 @@ end
 
 def team_colors(team_name)
   if game_hash[:home][:team_name].eql?(team_name)
-    return game_hash[:home][:team_name][:colors].join(", ")
+    return game_hash[:home][:colors]
   else
-    return game_hash[:away][:team_name][:colors].join(", ")
+    return game_hash[:away][:colors]
   end
 end
 
