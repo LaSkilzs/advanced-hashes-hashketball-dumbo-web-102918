8f57bf04fa5a759fd5ad8d5696c0e5f375d4914a
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-14 15:38:34 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,17 @@
 def game_hash
+  game = {
+      :home => {
+        :team_name => "",
+        :colors => [],
+        :players = {
+           :names = ""
+        }
+        
+      },
+      :away => {
+        
+      }
+  }
 end
 
 
