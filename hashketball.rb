95ac45221a66529549f3e5fcafc36cba30393ca3
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-14 16:11:20 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,6 +13,16 @@ def game_hash
             :steals => 3,
             :blocks => 1,
             :slam_dunks => 1
+          }, 
+          "Reggie Evans" => {
+            :number => 30,
+            :shoe => 14,
+            :points => 12,
+            :rebounds => 12, 
+            :assists => 12,
+            :steals => 12,
+            :blocks => 12,
+            :slam_dunks => 7
           }
       }
     },
@@ -21,15 +31,15 @@ def game_hash
         :team_name => "Charlotte Hornets",
         :colors => ["Turquoise", "Purple"],
         :players => {
-          "Alan Anderson" => {
-            :number => 0,
-            :shoe => 16,
-            :points => 22,
-            :rebounds => 12,
-            :assists => 12,
-            :steals => 3,
-            :blocks => 1,
-            :slam_dunks => 1
+          "Jeff Adrien" => {
+            :number => 4,
+            :shoe => 18,
+            :points => 10,
+            :rebounds => 1,
+            :assists => 1,
+            :steals => 2,
+            :blocks => 7,
+            :slam_dunks => 2
           }, 
           "Reggie Evans" => {
             :number => 30,
