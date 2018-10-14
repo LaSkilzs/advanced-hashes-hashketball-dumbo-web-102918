3b47456679cf26fa0a65eefcd2caef682c23ffc5
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-14 15:52:38 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,18 +1,18 @@
 def game_hash
   game = {
       :home => {
-        :team_name => "Brooklyn Nets",
-        :colors => ["Black", "White"],
-        :players => {
-           "Alan Anderson" = {
-             :number => 0,
-             :shoe => 16,
-             :points => 22,
-             :rebounds => 12,
-             :assists => 12,
-             :steals => 3,
-             :blocks => 1,
-             :slams_dunks => 1
+        # :team_name => "Brooklyn Nets",
+        # :colors => ["Black", "White"],
+        # :players => {
+        #   "Alan Anderson" = {
+        #     :number => 0,
+        #     :shoe => 16,
+        #     :points => 22,
+        #     :rebounds => 12,
+        #     :assists => 12,
+        #     :steals => 3,
+        #     :blocks => 1,
+        #     :slams_dunks => 1
            },
            "Reggie Evans" = {
              :number => 30,
